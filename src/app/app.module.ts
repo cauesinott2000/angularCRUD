e17ccfef2c70fd92318f1470d8987d773d0f3242
cauesinott2000 <cauesinott123@gmail.com>
2023-11-01 18:19:34 -0300

    material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+/**
+ * Material Conponents
+ */
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [{
   path:'', component:LoginFormComponent
@@ -20,7 +31,12 @@ const routes: Routes = [{
   imports: [
     BrowserModule,
     ReactiveFormsModule, 
-    RouterModule.forRoot(routes), BrowserAnimationsModule
+    RouterModule.forRoot(routes), BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatCardModule
   ],
   exports: [
     RouterModule
