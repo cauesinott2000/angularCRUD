09125be01e325f842d290372eeec95f8c8219339
Cauê Andrade <103803531+cauesinott2000@users.noreply.github.com>
2023-11-01 16:53:47 +0000

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const routes: Routes = [{
+  path:'', component:LoginFormComponent
+}];
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,11 @@ import { ProfileEditorComponent } from './profile-editor/profile-editor.componen
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, 
+    RouterModule.forRoot(routes), BrowserAnimationsModule
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
