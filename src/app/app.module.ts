c828f7370d79f0feaec45dbf30afcc13482908d0
caue <cauesinott123@gmail.com>
2023-11-06 20:52:35 -0300

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router'; 
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 /**
  * Material Conponents
  */
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
-const routes: Routes = [{
-  path:'', component:LoginFormComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginFormComponent,
+  },
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginFormComponent,
-    ProfileEditorComponent
-  ],
+  declarations: [AppComponent, LoginFormComponent, ProfileEditorComponent],
   imports: [
     BrowserModule,
-    ReactiveFormsModule, 
-    RouterModule.forRoot(routes), BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
     FlexLayoutModule,
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatButtonModule, 
-    MatCardModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
   ],
-  exports: [
-    RouterModule
-  ],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
